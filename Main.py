a92d98ccc15381f2fdefa49201dd0aacd1a154ef
Sebamalicz <smali211@mtroyal.ca>
2024-02-13 22:20:34 -0700

    Bidirectional Search Working
    FINALLY! ITS WORKING!

--- Main.py
+++ Main.py
@@ -5,9 +5,7 @@ from time import time
 cube = Cube()
 final_cube = Cube()
 cube.print_cube()
-#cube.print_percent_complete()
 start = time()
-#cube.solve_cube_outer(final_cube)
 for depth_limit in range(1, cube.max_depth_limit + 1):
             cube.max_depth = depth_limit
             final_cube.max_depth = depth_limit
@@ -15,44 +13,23 @@ for depth_limit in range(1, cube.max_depth_limit + 1):
             final_cube.memo.clear()
             cube.move_set = []
             final_cube.move_set = []
-            #print("Depth: ", depth_limit, final_cube.max_depth)
             if cube.solve_cube(0, None, final_cube):
-                #print("Forward works")
-                #print("self cube")
-                #cube.print_cube()
-                common_keys = set(cube.memo.keys()) & set(final_cube.memo.keys())
-
-                if common_keys:
-                    print("Common keys: ", common_keys)
-                else:
-                    print("No common keys")
-                    #print(type(self.memo))
-                    print(final_cube.memo)
+                cube.move_set.reverse()
+                for move in final_cube.final_move_set:
+                    revised_move = cube.convert_move(move)
+                    cube.apply_move(revised_move)
+                    cube.move_set.append(revised_move)
                 break
             if final_cube.reverse_solve_cube(0, None, cube):
-                print("Reverse works")
-                print(final_cube.common_keys)
-                print("Initial cube moveset:", cube.move_set)
-                for move in final_cube.move_set:
-                    cube.move_set.append(move)
-                cube.apply_rem_moves(final_cube.move_set)
-                break
+                for moves in final_cube.move_set:
+                    final_cube.undo_move(moves)
+                final_cube.print_cube()
+                final_cube.memo.clear()
+                final_cube.move_set = []
+                if final_cube.reverse_solve_cube_v2(0, None):
+                    final_cube.final_move_set = final_cube.move_set
 
 end = time()
-#cube.left_vertical_up()
-#cube.percent_of_front_solved()
-print("THIS IS THE INITIAL CUBE")
 cube.print_cube()
-print("Initial cube moveset:", cube.move_set)
-print("Final cube moveset:", final_cube.move_set)
+print("cube moveset:", cube.move_set)
 print ("Time: ", end - start)
-#final_cube.right_vertical_down()
-final_cube.print_cube()
-#b r w
-#o y g
-#y r b
-#b o w
-#w g r
-#w g o
-#y r g
-#o y b
--- Rubiks.py
+++ Rubiks.py
@@ -8,36 +8,31 @@ class Cube:
         self.max_depth_limit = 15
         self.max_depth = 0
         self.move_set = []
+        self.final_move_set = []
         self.memo = {}
-        self.common_keys = False
-
+        self.common_keys = None
 
     def is_solved(self):
-        #Create 6 functions that check if each side is solved
         if self.front_side_solved() and self.back_side_solved() and self.top_side_solved() and self.bottom_side_solved() and self.right_side_solved() and self.left_side_solved():
             return True
         return False
 
     def front_side_solved(self):
-        # This will be the function that checks if the front side is solved
         if self.cube[0].colors[0] == self.cube[1].colors[0] == self.cube[4].colors[0] == self.cube[5].colors[0]:
             return True
         return False
     
     def back_side_solved(self):
-        # This will be the function that checks if the back side is solved
         if self.cube[2].colors[0] == self.cube[3].colors[0] == self.cube[6].colors[0] == self.cube[7].colors[0]:
             return True
         return False
     
     def top_side_solved(self):
-        # This will be the function that checks if the top side is solved
         if self.cube[0].colors[1] == self.cube[1].colors[1] == self.cube[2].colors[1] == self.cube[3].colors[1]:
             return True
         return False
     
     def bottom_side_solved(self):
-        # This will be the function that checks if the bottom side is solved
         if self.cube[4].colors[1] == self.cube[5].colors[1] == self.cube[6].colors[1] == self.cube[7].colors[1]:
             return True
         return False
@@ -51,23 +46,11 @@ class Cube:
         if self.cube[1].colors[2] == self.cube[2].colors[2] == self.cube[5].colors[2] == self.cube[6].colors[2]:
             return True
         return False
-
-    def get_corner(self, corner):
-        return self.cube[corner]
-
-    def set_corner(self, corner, colors):
-        self.cube[corner] = Corner(colors)
-
-    def print_final_cube(self):
-        for corner in self.final_cube:
-            print(corner)
    
     def set_initial_state(self):
         print("Would you like to input a file ? (Y/N)")
         corners = []
-
         choice = input().strip().upper()
-
         if choice == "Y":
             print("Enter the file name: ")
             file_name = input().strip()
@@ -81,14 +64,11 @@ class Cube:
                         print("Invalid corner colors. Please try again.")
                         break
             return corners
-        else:
-        
+        else:  
             print("Enter the colors of each corner on the cube.")
             print("Use W for White, G for Green, R for Red, B for Blue, O for Orange, and Y for Yellow.")
             print("Ensure to keep mind of the front of the cube at all points.")
             print("Enter the colors in the format: Color1 Color2 Color3")
-    
-
             for corner in CORNERS:
                 while True:
                     corner_colors = input("Enter the colors of the " + corner + " corner: ").strip().upper().split()
@@ -106,7 +86,6 @@ class Cube:
             print(corner.colors)
 
     def right_vertical_up(self):
-    
         self.swap_first_two(self.cube[0])
         self.swap_first_two(self.cube[3])
         self.swap_first_two(self.cube[4])
@@ -119,9 +98,7 @@ class Cube:
         self.cube[7] = self.cube[3]
         self.cube[3] = temp_cube0
 
-  
     def right_vertical_down(self):
-
         self.swap_first_two(self.cube[0])
         self.swap_first_two(self.cube[3])
         self.swap_first_two(self.cube[4])
@@ -134,9 +111,7 @@ class Cube:
         self.cube[7] = self.cube[4]
         self.cube[4] = temp_cube0
 
- 
     def left_vertical_up(self):
-
         self.swap_first_two(self.cube[1])
         self.swap_first_two(self.cube[2])
         self.swap_first_two(self.cube[5])
@@ -150,7 +125,6 @@ class Cube:
         self.cube[2] = temp_cube0
 
     def left_vertical_down(self):
-
         self.swap_first_two(self.cube[1])
         self.swap_first_two(self.cube[2])
         self.swap_first_two(self.cube[5])
@@ -164,7 +138,6 @@ class Cube:
         self.cube[5] = temp_cube0
 
     def top_horizontal_right(self):
-
         self.swap_first_last(self.cube[0])
         self.swap_first_last(self.cube[1])
         self.swap_first_last(self.cube[2])
@@ -178,7 +151,6 @@ class Cube:
         self.cube[3] = temp_cube0
     
     def top_horizontal_left(self):
-
         self.swap_first_last(self.cube[0])
         self.swap_first_last(self.cube[1])
         self.swap_first_last(self.cube[2])
@@ -192,7 +164,6 @@ class Cube:
         self.cube[1] = temp_cube0
 
     def bottom_horizontal_right(self):
-
         self.swap_first_last(self.cube[4])
         self.swap_first_last(self.cube[5])
         self.swap_first_last(self.cube[6])
@@ -206,7 +177,6 @@ class Cube:
         self.cube[7] = temp_cube0
 
     def bottom_horizontal_left(self):
-
         self.swap_first_last(self.cube[4])
         self.swap_first_last(self.cube[5])
         self.swap_first_last(self.cube[6])
@@ -220,7 +190,6 @@ class Cube:
         self.cube[5] = temp_cube0
 
     def front_clockwise(self):
-
         self.swap_last_two(self.cube[0])
         self.swap_last_two(self.cube[1])
         self.swap_last_two(self.cube[4])
@@ -234,7 +203,6 @@ class Cube:
         self.cube[4] = temp_cube0
 
     def front_counter_clockwise(self):
-
         self.swap_last_two(self.cube[0])
         self.swap_last_two(self.cube[1])
         self.swap_last_two(self.cube[4])
@@ -248,21 +216,6 @@ class Cube:
         self.cube[1] = temp_cube0
 
     def back_clockwise(self):
-
-        self.swap_last_two(self.cube[2])
-        self.swap_last_two(self.cube[3])
-        self.swap_last_two(self.cube[6])
-        self.swap_last_two(self.cube[7])
-
-        temp_cube0 = self.cube[2]
-
-        self.cube[2] = self.cube[3]
-        self.cube[3] = self.cube[7]
-        self.cube[7] = self.cube[6]
-        self.cube[6] = temp_cube0
-
-    def back_counter_clockwise(self):
-
         self.swap_last_two(self.cube[2])
         self.swap_last_two(self.cube[3])
         self.swap_last_two(self.cube[6])
@@ -275,6 +228,19 @@ class Cube:
         self.cube[7] = self.cube[3]
         self.cube[3] = temp_cube0
 
+
+    def back_counter_clockwise(self):
+        self.swap_last_two(self.cube[2])
+        self.swap_last_two(self.cube[3])
+        self.swap_last_two(self.cube[6])
+        self.swap_last_two(self.cube[7])
+
+        temp_cube0 = self.cube[2]
+
+        self.cube[2] = self.cube[3]
+        self.cube[3] = self.cube[7]
+        self.cube[7] = self.cube[6]
+        self.cube[6] = temp_cube0
 
     def swap_first_two(self, corner):
         temp = corner.colors[0]
@@ -291,45 +257,22 @@ class Cube:
         corner.colors[0] = corner.colors[2]
         corner.colors[2] = temp
 
-    def solve_cube_outer(self, final_cube):
+    def solve_cube_outer(self):
         for depth_limit in range(1, self.max_depth_limit + 1):
             self.max_depth = depth_limit
             self.memo.clear()
             self.move_set = []
-            if self.solve_cube(0, None):
-                #print("Forward works")
-                #print("self cube")
-                #self.print_cube()
-                common_keys = set(self.memo.keys()) & set(final_cube.memo.keys())
-
-                if common_keys:
-                    print("Common keys: ", common_keys)
-                else:
-                    print("No common keys")
-                    #print(type(self.memo))
-                    print(final_cube.memo)
-                return True
-            if self.reverse_solve_cube(0, None, final_cube):
-                print("Reverse works")
-                self.move_set.append(final_cube.move_set)
+            if self.solve_cube(0):
                 return True
         return False
 
-    # The problem is that reverse function returns true first before solve cube does
-    # which means that the moves made on the initial cube are not being appened to the move set
-    # because the moves are only appened when solve cube returns true
     def solve_cube(self, depth = 0, last_move = None, final_cube = None):
         cube_key = self.cube_state_to_key()
-        #print("Depth: ", depth)
-        #print("moveset: ", self.move_set)
 
         if cube_key in self.memo and self.memo[cube_key] == depth:
-            #return self.memo[cube_key]
             return False
 
-        # should we add a check here to see if the cube memo key equals the final cube key?
-        #common_keys = set(self.memo.keys()) & set(final_cube.memo.keys())
-        if self.is_solved() or final_cube.common_keys:
+        if self.is_solved() or cube_key == final_cube.common_keys:
             self.memo[cube_key] = depth
             self.common_keys = True
             return True
@@ -338,13 +281,11 @@ class Cube:
             return False
 
         solution_found = False
-        for move in self.best_move(last_move):
-        #self.possible_moves(last_move):
+        for move in self.possible_moves(last_move):
             self.apply_move(move)
             if self.solve_cube(depth + 1, move, final_cube):
                 self.move_set.append(move)
                 solution_found = True
-                print("In solve cube, initial moveset is", self.move_set)
                 break
             self.undo_move(move)
         
@@ -353,49 +294,57 @@ class Cube:
         return solution_found
 
     def reverse_solve_cube(self, depth = 0, last_move = None, cube = None):
-        # I think we will need the final cube to have it's own memo 
-        #print("In reverse solve cube")
         cube_key = self.cube_state_to_key()
-        #print("Cube key: ", cube_key)
-        #print("Depth: ", depth)
-        # apply the move to solved cube
 
         if cube_key in self.memo and self.memo[cube_key] == depth:
-            #return self.memo[cube_key]
-            #print("cube key in memo and depth is the same", depth)
             return False
 
-        # we should check if the solved_cube == the current cube, if they are equal then we have solved the cube
         common_keys = set(self.memo.keys()) & set(cube.memo.keys())
 
         if common_keys:
-            self.common_keys = True
-            print("Common keys: ", common_keys)
-            #print("Initial cube keys: ", cube.memo)
-            #print("Final cube keys: ", self.memo)
-            #print(cube.move_set)
+            self.common_keys = list(common_keys)[0]
             self.memo[cube_key] = depth
             return True
         
         if depth >= self.max_depth:
-            #print(self.max_depth)
             return False
 
         solution_found = False
-        for move in Constants.MOVES:
-        #self.possible_moves(last_move):
+        for move in self.possible_moves(last_move):
             self.apply_move(move)
-            #print("After move for final cube")
-            #self.print_cube()
             if self.reverse_solve_cube(depth + 1, move, cube):
                 self.move_set.append(move)
                 solution_found = True
-                print("In reverse move, final cube moveset is", self.move_set)
                 break
             self.undo_move(move)
         
         if not solution_found:
-            #print("Solution not found")
+            self.memo[cube_key] = depth
+        return solution_found
+    
+    def reverse_solve_cube_v2(self, depth = 0, last_move = None):
+        cube_key = self.cube_state_to_key()
+
+        if cube_key in self.memo and self.memo[cube_key] == depth:
+            return False
+
+        if cube_key == self.common_keys:
+            self.memo[cube_key] = depth
+            return True
+        
+        if depth >= self.max_depth:
+            return False
+
+        solution_found = False
+        for move in self.possible_moves(last_move):
+            self.apply_move(move)
+            if self.reverse_solve_cube_v2(depth + 1, move):
+                self.move_set.append(move)
+                solution_found = True
+                break
+            self.undo_move(move)
+        
+        if not solution_found:
             self.memo[cube_key] = depth
         return solution_found
     
@@ -457,19 +406,40 @@ class Cube:
             case _: return []
     
     def cube_state_to_key(self):
-        key = '|'.join(''.join(corner.colors) for corner in self.cube)
+        key_dict = {}
+        position = 1
+        key = "" 
+        for corner in self.cube:
+            for color in corner.colors:
+                if color not in key_dict:
+                    key_dict[color] = position
+                    position += 1
+                key += str(key_dict[color])
+
         return key
 
+    def convert_move(self, move):
+        match move:
+            case Constants.RVU: return Constants.RVD 
+            case Constants.RVD: return Constants.RVU
+            case Constants.LVU: return Constants.LVD
+            case Constants.LVD: return Constants.LVU
+            case Constants.THR: return Constants.THL
+            case Constants.THL: return Constants.THR
+            case Constants.BHR: return Constants.BHL
+            case Constants.BHL: return Constants.BHR
+            case Constants.FC: return Constants.FCC 
+            case Constants.FCC: return Constants.FC
+            case Constants.BC: return Constants.BCC
+            case Constants.BCC: return Constants.BC
+    
     def best_move(self, last_move=None):
         best_moves = []
         for move in self.possible_moves(last_move):
             self.apply_move(move)
             new_percent = self.percent_solved()
-            best_moves.append((move, new_percent))  # Store both move and its result
-
+            best_moves.append((move, new_percent))  
             self.undo_move(move)
-        #print("best moves: ", best_moves)
-        # Sort the moves based on the percentage solved, in descending order
         best_moves.sort(key=lambda x: x[1], reverse=True)
         sorted_moves = [move[0] for move in best_moves]
         return sorted_moves
@@ -480,7 +450,6 @@ class Cube:
     
     # Helper function to calculate the % of each side that is solved
     def percent_of_front_solved(self):
-        # This will be the function that checks if the front side is solved
         front_count = 0
         if self.cube[0].colors[0] == self.cube[1].colors[0]:
             front_count += 1
@@ -639,4 +608,3 @@ class Cube:
         
         #print("Left count: ", str(left_count), "Left %: ", str((left_count / 4) * 100))
         return (left_count / 4) * 100
-    
